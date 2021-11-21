9b2c44eef8617b3cf4230e4864a74fecb2154b12
Romeyn <support@swiftpac.com>
2021-11-21 01:42:29 -0400

    Design home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,78 @@
 import React from 'react';
-import banner from '../images/banner.jpg'
+import Geography from '../images/Geography.svg'
+import Devops from '../images/Devops.svg'
+import Teacher from '../images/Teacher.svg'
+import CardImage from '../images/13236360.png'
 const Home = (props) => {
     return ( 
         <>
-               <section class="section">
-            <div className="container">
-  <h1 class="title">Magical!</h1>
-  <h2 class="subtitle">
-        Using the magic of code!
-  </h2>
+              <div className="hero">
+                    <div className="content">
+                          <h1>EMBRACING THE UNKNOWN</h1>
+                          <p>Meet Romeyn</p>
+                    </div>
+                    <svg className="hero-design" width="100" height="100" viewBox="0 0 100 100" fill="none" >
+                  <rect className="square-anim" width="100" height="100" fill="#F9866F"/>
+                  <rect className="square-anim" width="100" height="100" fill="#F9866F"/>
+                  <rect className="square-anim" width="100" height="100" fill="#F9866F"/>
+                  <rect className="square-anim" width="100" height="100" fill="#F9866F"/>
+                  <rect className="square-anim" width="100" height="100" fill="#F9866F"/>
 
+                  </svg>
+                  <svg className="triangle-anim hero-design2" width="329" height="100" viewBox="0 0 329 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 0L328.5 47.2906L0 100V0Z" fill="#F9866F"/>
+</svg>
+                  <svg className="hero-design-canvas hero-design3" width="529" height="500" viewBox="0 0 529 500" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="0.5" y="0.5" width="528" height="499" fill="#F2F2F2" stroke="#596C73"/>
+</svg>
 
-            </div>
-</section>
-        <section class="section">
-  <div class="container">
-  
-  <img src={banner} alt="Cards"/>
+                    
+              </div>
+              <section className="featured">
+                    
+      <img className="left transition2 featured-img" src={CardImage} alt="" />
+                    
+                    <div className="right">
+                          <div className="inner-transition2">
+                                <p className="subtitle">featured project</p>
+                                <a href="#" className="featured-title">Card Magic Game</a>
+                                <p className="featured-desc">Intelligent card game that guesses your card!</p>
+                          </div>
+                    </div>
+              </section>
+              <section className="skills">
+                  <div className="skills-container">
+                        <ul>
+                              <li className="transition2">
+                                    <div className="icon-container one">
+                                          <img src={Geography} alt="" />
+                                    </div>
+                                    <p className="skill-title">Web developer</p>
+                                    <p className="featured-desc skill-desc">I make webapps using Javascript</p>
+                              </li>
+                              <li className="transition2">
+                                    <div className="icon-container two">
+                                          <img src={Devops} alt="" />
+                                    </div>
+                                    <p className="skill-title">Web developer</p>
+                                    <p className="featured-desc skill-desc">I make webapps using Javascript</p>
+                              </li>
+                              <li className="transition2">
+                                    <div className="icon-container three">
+                                          <img src={Teacher} alt="" />
+                                    </div>
+                                    <p className="skill-title">Web developer</p>
+                                    <p className="featured-desc skill-desc">I make webapps using Javascript</p>
+                              </li>
+                        </ul>
+                  </div>
+              </section>
+              <section className="call-to-action">
+                    <div className="cta-container transition3">
+                           <a href="#">Got a job? Contact Me</a>
+                    </div>
 
-  </div>
-</section>
-<section class="section">
-            <div className="container">
-  <h1 class="title">Magical!</h1>
-  <h2 class="subtitle">
-        Using the magic of code!
-  </h2>
-
-
-            </div>
-</section>
+              </section>
  
         </>
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,30 +2,24 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 const Nav = (props) => {
     return ( 
-
-        <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-  <div className="navbar-brand">
-  <Link className="navbar-item" to= "/"> MATHEMAGIC</Link>
-
-    
-  </div>
-
-  <div id="navbarBasicExample" className="navbar-menu">
-    <div className="navbar-start">
-    <Link className="navbar-item" to= "/"> Home</Link>
+      <div className="nav-container">
+        <header>
+      <Link className="logo" to= "/"> NoobMyster</Link>
       
-                
+          <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
 
-      <Link className="navbar-item" to= "/Magic"> Tricks</Link>
-      <Link className="navbar-item" to= "/Magic"> About</Link>
-  
 
-     
-      </div>
-    </div>
+          <ul id="navbarBasicExample" className="navbar-menu">
+            <li><Link className="navbar-item" to= "/"> Home</Link></li>
+            <li><Link className="navbar-item" to= "/Magic"> Projects</Link></li>
+            <li><Link className="navbar-item" to= "/Magic"> Blog</Link></li>
+            <li><Link className="navbar-item" to= "/Magic"> About</Link></li>
+          </ul>
 
    
 </nav>
+</header>
+</div>
     
      
       
